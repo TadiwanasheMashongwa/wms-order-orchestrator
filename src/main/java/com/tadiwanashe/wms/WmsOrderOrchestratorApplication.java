534fefdbd9b85cb7e41d5cba4d0e629bed9edb84
TadiwansheMashongwa <mashongwatadiwanashe@gmail.com>
2026-04-07 07:49:57 +0200

    chore: initial WMS Order Orchestrator setup

---
+++ src/main/java/com/tadiwanashe/wms/WmsOrderOrchestratorApplication.java
@@ -0,0 +1,13 @@
+package com.tadiwanashe.wms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WmsOrderOrchestratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WmsOrderOrchestratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tadiwanashe/wms/WmsOrderOrchestratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tadiwanashe.wms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class WmsOrderOrchestratorApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+}
