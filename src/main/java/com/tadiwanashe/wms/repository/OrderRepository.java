97a9cc8d8529c7ca025eb0ecb510d19a51621e15
TadiwansheMashongwa <mashongwatadiwanashe@gmail.com>
2026-04-07 08:51:26 +0200

    feat(repository): add OrderRepository with findByCustomerId and findByStatus

---
+++ src/main/java/com/tadiwanashe/wms/repository/OrderRepository.java
@@ -0,0 +1,11 @@
+package com.tadiwanashe.wms.repository;
+
+import com.tadiwanashe.wms.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface OrderRepository extends JpaRepository<Order,Long> {
+    List<Order>  findByCustomerId(String CustomerId);
+    List<Order> findStatus(String Status);
+}
